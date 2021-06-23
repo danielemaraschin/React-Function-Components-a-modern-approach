687c5b8b65aeb3e051ad7f61fe62873d1fa944c7
danielemaraschin <danikmaraschin@gmail.com>
2021-06-23 18:42:00 +0200

    Learning about MATERIAL UI

--- my-app/src/components/FormularioCadastro/FormularioCadastro.js
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Button from "@material-ui/core/Button"
-
+import { TextField } from '@material-ui/core';
 
 function FormularioCadastro(){
     return(
@@ -20,7 +20,7 @@ function FormularioCadastro(){
         <label>Novidades</label>
         <input type="checkbox"/>
 
-        <Button type="submit" variant="contained" color="primary">
+        <Button type="submit" variant="contained" color="primary"> {/*on material-ui.com has all kind of tags we can get style */}
             Cadastrar
         </Button>
     </form>
