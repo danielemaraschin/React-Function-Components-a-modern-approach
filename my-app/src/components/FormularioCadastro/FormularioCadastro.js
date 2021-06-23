3905f2e3c2915b33daf09aa7fcdb46575bae9f88
danielemaraschin <danikmaraschin@gmail.com>
2021-06-23 17:50:39 +0200

    working with form

--- my-app/src/components/FormularioCadastro/FormularioCadastro.js
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 function FormularioCadastro(){
     return(
     <form>
+        <label>Nome</label>
         <input type="text"/>
+        
+        <label>Sobrenome</label>
+        <input type="text"/>
+
+        <label>CPF</label>
+        <input type="text"/>
+        
+
     </form>);
 }
 
