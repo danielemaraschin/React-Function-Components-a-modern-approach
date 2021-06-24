9acd2f743615f97c09a4bbe08190fd55c89b3b7a
danielemaraschin <danikmaraschin@gmail.com>
2021-06-24 13:17:21 +0200

    Working with materia UI - margin

--- my-app/src/components/FormularioCadastro/FormularioCadastro.js
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.js
@@ -7,9 +7,9 @@ function FormularioCadastro(){
     <form>
         <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth/>
         
-        <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth/>
+        <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
 
-        <TextField id="cpf" label="CPF" variant="outlined" fullWidth/>
+        <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth/>
         
         <label>Promoções</label>
         <input type="checkbox"/>
