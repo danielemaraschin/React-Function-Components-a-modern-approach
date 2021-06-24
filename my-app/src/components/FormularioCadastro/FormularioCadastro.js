c7de48c4b8c69c889ca68feda00dcd1ab61c2300
danielemaraschin <danikmaraschin@gmail.com>
2021-06-24 14:12:42 +0200

    Learning about switch and FormControlLabel

--- my-app/src/components/FormularioCadastro/FormularioCadastro.js
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import Button from "@material-ui/core/Button"
-import {TextField} from '@material-ui/core';
+import { TextField, Button, Switch, FormControlLabel } from '@material-ui/core';
 
-function FormularioCadastro(){
-    return(
-    <form>
-        <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth/>
-        
-        <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
+function FormularioCadastro() {
+    return (
+        <form>
+            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
+            <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
+            <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
 
-        <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth/>
+            <FormControlLabel label="Promoções" control={<Switch name="promocoes" label="Promoções" defaultChecked color="primary" />}/>
+            <FormControlLabel label="Novidades" control={<Switch name="novidades"  defaultChecked color="primary"/>}/>
         
-        <label>Promoções</label>
-        <input type="checkbox"/>
-        
-        <label>Novidades</label>
-        <input type="checkbox"/>
-
-        <Button type="submit" variant="contained" color="primary"> {/*on material-ui.com has all kind of tags we can get style */}
-            Cadastrar
-        </Button>
-    </form>
-   );
+            <Button type="submit" variant="contained" color="primary"> {/*on material-ui.com has all kind of tags we can get style */}
+                Cadastrar
+            </Button>
+        </form>
+    );
 }
 
 export default FormularioCadastro;
