7cfaae6bf79da79afdc4c273ed96e6444d448ca1
danielemaraschin <danikmaraschin@gmail.com>
2021-06-24 09:47:37 +0200

    Working with maxWidth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
   render() {
     return (
-      <Container component="article" >
+      <Container component="article" maxWidth="sm">
         <h1>Formulário de cadastro</h1>
         <FormularioCadastro />
       </Container>
