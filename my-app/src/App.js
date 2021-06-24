48f6c4d26e1cfe2a0a76526d9566d4df71841010
danielemaraschin <danikmaraschin@gmail.com>
2021-06-24 14:56:16 +0200

    Learning about Material UI Typography

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import '@fontsource/roboto';
 import {Container, Typography} from "@material-ui/core";
+
 
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm">
-        <Typography variant="h1">Formulário de cadastro</Typography>
+        <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
         <FormularioCadastro />
       </Container>
     );
