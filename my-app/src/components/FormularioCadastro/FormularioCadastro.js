c79117ef020aea78951bee46b24b4fe7211bbdb9
danielemaraschin <danikmaraschin@gmail.com>
2021-06-26 09:56:56 +0200

    Learning about hooks

--- my-app/src/components/FormularioCadastro/FormularioCadastro.js
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.js
@@ -2,38 +2,41 @@ import React, { useState } from 'react';
 import { TextField, Button, Switch, FormControlLabel } from '@material-ui/core';
 
 function FormularioCadastro() {
-    const nome = useState("Dani");
+    const [nome, setNome] = useState("Dani");
     return (
-        <form onSubmit={ (event) => {
+        <form onSubmit={(event) => {
             event.preventDefault();
-            console.log(nome)}}>
-            <TextField 
-                onChange={ event => {
-                    nome = (event.target.value);
-                } }
+            console.log(nome);
+        }}
+        >
+            <TextField
+                value={nome}
+                onChange={(event) => {
+                    setNome = (event.target.value);
+                }}
                 id="nome"
-                label="Nome" 
+                label="Nome"
                 variant="outlined"
                 margin="normal"
                 fullWidth />
 
             <TextField
                 id="sobrenome"
-                label="Sobrenome" 
-                variant="outlined" 
-                margin="normal" 
+                label="Sobrenome"
+                variant="outlined"
+                margin="normal"
                 fullWidth />
 
             <TextField
                 id="cpf"
-                label="CPF" 
+                label="CPF"
                 variant="outlined"
                 margin="normal"
                 fullWidth />
-                            {/*esse formControlLabel está controlando esse interruptor */}
-            <FormControlLabel label="Promoções" control={<Switch name="promocoes" label="Promoções" defaultChecked color="primary" />}/>
-            <FormControlLabel label="Novidades" control={<Switch name="novidades"  defaultChecked color="primary"/>}/>
-        
+            {/*esse formControlLabel está controlando esse interruptor */}
+            <FormControlLabel label="Promoções" control={<Switch name="promocoes" label="Promoções" defaultChecked color="primary" />} />
+            <FormControlLabel label="Novidades" control={<Switch name="novidades" defaultChecked color="primary" />} />
+
             <Button type="submit" variant="contained" color="primary"> {/*on material-ui.com has all kind of tags we can get style */}
                 Cadastrar
             </Button>
